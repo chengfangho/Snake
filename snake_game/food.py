ec7a520f26fa7ac950ef1a9f5ec335b8ec638096
chengfangho <ho05646@gmail.com>
2023-07-04 15:31:48 -0700

    snake

--- snake_game/food.py
+++ snake_game/food.py
@@ -10,6 +10,6 @@ class Food(Turtle):
         self.speed("fastest")
         self.refresh()
     def refresh(self):
-        random_x = random.randrange(-280, 280, 20)
-        random_y = random.randrange(-280, 280, 20)
+        random_x = random.randrange(-260, 260, 20)
+        random_y = random.randrange(-260, 260, 20)
         self.goto(random_x, random_y)
